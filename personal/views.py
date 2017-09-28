e5c844e6aeb636bd1ecd0a9fa55cc732232b4f7b
VPZSU <vasia.petrivsky@gmail.com>
2017-09-28 15:50:34 +0300

    new page

--- personal/views.py
+++ personal/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse, request
 
 def index(request):
     return render(request, 'personal/home.html')
 
+def contact(request):
+    return render(request, 'personal/basic.html', {'content':['If you would like to contact me, please email', 'email@dot.com']})
